79712b2ac706870e02136d5e7757097fe3c8f627
frediromo <frediromo@gmail.com>
2023-10-16 08:56:50 -0600

    Added inorder, preorder and postorder

---
+++ cmake-build-debug/CMakeFiles/BinaryTrees.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/fredi/Documents/DS_And_Algorithms/BST/MyBST.cpp" "CMakeFiles/BinaryTrees.dir/MyBST.cpp.o" "gcc" "CMakeFiles/BinaryTrees.dir/MyBST.cpp.o.d"
+  "/home/fredi/Documents/DS_And_Algorithms/BST/main.cpp" "CMakeFiles/BinaryTrees.dir/main.cpp.o" "gcc" "CMakeFiles/BinaryTrees.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
